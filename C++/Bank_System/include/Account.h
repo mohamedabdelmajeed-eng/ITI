2a384f17460719546dc88126079b858f6b0d7352
mohamed <mohamed.abdelmajeed@msa.edu.eg>
2026-07-30 16:28:35 +0300

    include account.h done

---
+++ C++/Bank_System/include/Account.h
@@ -0,0 +1,33 @@
+// (Header file) 
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <string>
+
+class Account {
+protected:
+    std::string accountId;
+    std::string ownerName;
+    double balance;
+
+public:
+    Account(
+        const std::string& accountId,
+        const std::string& ownerName,
+        double balance
+    );
+
+    virtual ~Account() = default;
+
+    std::string getAccountId() const;
+    std::string getOwnerName() const;
+    double getBalance() const;
+
+    void deposit(double amount);
+    virtual void withdraw(double amount);
+
+    virtual std::string getAccountType() const = 0;
+    virtual void display() const = 0;
+};
+
+#endif
